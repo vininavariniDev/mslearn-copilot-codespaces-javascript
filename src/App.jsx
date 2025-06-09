694a27e478d453e9ee43bc39e8c3328182de8224
vininavariniDev <vininavarini9@hotmail.com>
2025-06-09 13:31:44 +0000

    Atualiza informações de contato e adiciona animação para ícones sociais

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Teste Grenier",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "teste@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
